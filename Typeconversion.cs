1718b7c802659385836693ed9620eb546ceb5355
JithendraVishnu <36967135+JithendraVishnu@users.noreply.github.com>
2020-06-05 21:29:27 +0530

    Add files via upload

---
+++ Typeconversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter3
+{
+    class Typeconversion
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, ch = 122;
+            short s;
+            decimal dec = 1.22222m;
+            long l;
+            char charval;
+            s = (short)i;
+            l = (long)dec;
+            charval = (char)ch;
+            Console.WriteLine("short {0} , long {1} , char {2} ", s, l, charval);
+        }
+    }
+}
