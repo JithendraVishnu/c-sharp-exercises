74a6bccd8849471e33508f661205225f8d303bfd
JithendraVishnu <36967135+JithendraVishnu@users.noreply.github.com>
2020-06-05 20:58:10 +0530

    Add files via upload

---
+++ Datatype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Datatype
+    {
+        static void Main()
+        {
+            short s;
+            float f;
+            s = 10;
+            f = 1.876f;
+            Console.WriteLine("short {0} float {1}", s, f);
+        }
+    }
+
+}
